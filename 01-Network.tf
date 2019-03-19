cfb7442f42612dae884a259465e20841eced0cb0
Daniel Sanchez <daniel_sanchez_d@hotmail.com>
2019-03-19 22:40:50 +0000

    Add basic networking 1

--- 01-Network.tf
+++ 01-Network.tf
@@ -1,3 +1,19 @@
 resource "aws_vpc" "test" {
   cidr_block = "10.0.0.0/16"
 }
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = "${aws_vpc.test.id}"
+
+  tags = {
+    Name = "Main"
+  }
+}
+resource "aws_subnet" "ec2" {
+  vpc_id     = "${aws_vpc.test.id}"
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "EC2"
+  }
+}
