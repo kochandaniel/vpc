faa325bcfc833e4989832875fdeb83058af85614
danielsanchezd <daniel_sanchez_d@hotmail.com>
2019-04-11 14:46:51 +0100

    Add subnets, route table, security group

--- 01-Network.tf
+++ 01-Network.tf
@@ -1,3 +1,5 @@
+### VPC Main
+
 resource "aws_vpc" "test" {
   cidr_block = "10.0.0.0/16"
 }
@@ -9,13 +11,71 @@ resource "aws_internet_gateway" "gw" {
     Name = "Main"
   }
 }
-resource "aws_subnet" "ec2" {
+
+### Subnets
+
+resource "aws_subnet" "external" {
+  vpc_id     = "${aws_vpc.test.id}"
+  cidr_block = "10.0.0.0/24"
+
+  tags = {
+    Name = "External Subnet"
+  }
+}
+
+resource "aws_subnet" "internal" {
   vpc_id     = "${aws_vpc.test.id}"
   cidr_block = "10.0.1.0/24"
 
   tags = {
-    Name = "EC2"
+    Name = "Internal Subnet"
   }
 }
 
-# Hallo
+## Route tables
+
+resource "aws_route_table" "external" {
+  vpc_id = "${aws_vpc.test.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.gw.id}"
+  }
+
+  tags = {
+    Name = "External"
+  }
+}
+
+resource "aws_route_table" "internal" {
+  vpc_id = "${aws_vpc.test.id}"
+
+  tags = {
+    Name = "Internal"
+  }
+}
+
+## Security Groups
+
+resource "aws_security_group" "ssh" {
+  name = "ssh"
+  vpc_id = "${aws_vpc.test.id}"
+
+  tags = {
+    Name = "Default SSH"
+  }
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+} 
