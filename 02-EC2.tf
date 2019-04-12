38b0a1dfdc16be6a68500f33c2ec474ec59a9b33
danielsanchezd <daniel_sanchez_d@hotmail.com>
2019-04-12 10:56:38 +0100

    Initial example release

--- 02-EC2.tf
+++ 02-EC2.tf
@@ -1,26 +1,24 @@
-## Latest CentOS 7.6 image
-
-data "aws_ami" "centos" {
-  most_recent = true
- 
-  filter {
-    name = "name"
-    values = ["CentOS 7.6 - 18.0.0.0SE*"]
-  }
-  owners = ["679593333241"]
-} 
-
 ## EC2 Instances
 ## Note: The keypair used for these instances was created through the UI for security purposes. The AMI is also hardcoded to keep consistency between existing and new instances.
 
-resource "aws_instance" "test" {
+resource "aws_instance" "external" {
   ami = "ami-09ead922c1dad67e4"
   instance_type = "t2.micro"
-  subnet_id = "${aws_subnet.external.id}"
+  subnet_id = "${aws_subnet.external1.id}"
   key_name = "Default"
   vpc_security_group_ids = ["${aws_security_group.ssh.id}"]
-
   tags = {
-    Name = "Test Instance"
+    Name = "External"
+  }
+}
+
+resource "aws_instance" "internal" {
+  ami = "ami-09ead922c1dad67e4"
+  instance_type = "t2.micro"
+  subnet_id = "${aws_subnet.internal1.id}"
+  key_name = "Default"
+  vpc_security_group_ids = ["${aws_security_group.ssh.id}"]
+  tags = {
+    Name = "Internal"
   }
 }
