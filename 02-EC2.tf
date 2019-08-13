aff5635caa083d27b196508651583913d9051e94
dkochan <Kiminotyze5>
2019-08-13 16:41:29 +0100

    Working on this one in progress

--- 02-EC2.tf
+++ 02-EC2.tf
@@ -1,13 +1,11 @@
-## EC2 Instances
-## Note: The keypair used for these instances was created through the UI for security purposes. The AMI is also hardcoded to keep consistency between existing and new instances.
+## EC2 Instances:
 
 resource "aws_instance" "external" {
   ami = "ami-86fe70f8"
   instance_type = "t3.micro"
   subnet_id = "${aws_subnet.external1.id}"
   key_name = "Dank"
-  vpc_security_group_ids = [
-    "${aws_security_group.ssh.id}"]
+  vpc_security_group_ids = ["${aws_security_group.ssh.id}"]
 
   tags = local.ec2_tags
 }
@@ -22,16 +20,6 @@ resource "aws_instance" "internal" {
   tags = local.ec2_tags
 }
 
-
-resource "aws_instance" "internal" {
-  ami = "ami-95b53beb"
-  instance_type = "t3.micro"
-  subnet_id = "${aws_subnet.internal1.id}"
-  key_name = "Dank"
-  vpc_security_group_ids = ["${aws_security_group.ssh.id}"]
-
- tags = local.ec2_tags
-}
 
 locals {
   ec2_tags = {
